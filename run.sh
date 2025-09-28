0d04b0185584ca2416b9bf121721fd9f60f554b5
giliover <giliover@hotmail.com>
2025-09-28 15:54:21 -0300

    refactor: change docker config to exec flag

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 project_name="sei-spa"
 
-function build() {
+function exec() {
     docker run -it --rm \
         -v ~/AndroidStudioProjects/CampoMinado/:/app \
         -v ~/Android/Sdk/:/Android/Sdk/ \
         -e ANDROID_HOME=/Android/Sdk \
         -w /app \
-          android-openjdk:24 bash gradlew assembleDebug 
+          android-openjdk:24 bash 
 
 }
 
 case $1 in
-"--build") build;;
+"--exec") exec;;
 *) echo "Opção Inválida!" ;;
 esac
