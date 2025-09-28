8e7bc8919a5e14d52856bef7a2fc2693dac76d23
giliover <giliover@hotmail.com>
2025-09-28 10:39:36 -0300

    chore: add splash with delay of 400ms

--- app/src/main/java/com/example/campominado/MainActivity.kt
+++ app/src/main/java/com/example/campominado/MainActivity.kt
@@ -13,17 +13,39 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.campominado.ui.theme.CampoMinadoTheme
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+
 class MainActivity : ComponentActivity() {
+
+    private var isLoading by mutableStateOf(true)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { isLoading }
+
+        lifecycleScope.launch {
+            delay(400)
+            isLoading = false
+        }
+
         enableEdgeToEdge()
+
         setContent {
-            CampoMinadoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            if (!isLoading) {
+                CampoMinadoTheme {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
